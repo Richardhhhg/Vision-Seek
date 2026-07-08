c2606979b19d756bc4299c738881470cc6e9843f
rhhg <richardh110604@gmail.com>
2026-07-08 17:36:28 -0400

    post processing needs tests

---
+++ src/detection/tests/test_post_processor.py
@@ -0,0 +1,6 @@
+# Tests for the post processor module
+# 1. Testing each post processing step individually
+# a. Test bw_to_color step
+# b. Test stitch step
+# 2. Testing the postprocess_video method with multiple steps
+# 3. Testing the postprocess_video method with no steps in the preprocessed video
