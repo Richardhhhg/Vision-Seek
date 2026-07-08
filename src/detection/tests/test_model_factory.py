e2198dc40d3ef31d5e39ce341f4f95c1ad342692
rhhg <richardh110604@gmail.com>
2026-07-08 17:31:54 -0400

    model factory tests

--- src/detection/tests/test_model_factory.py
+++ src/detection/tests/test_model_factory.py
@@ -1,3 +1,32 @@
 # Tests for making the model
 # 1. Making sure can make valid model
 # 2. Testing with invalid model type, make sure it behaves as expected
+import pytest
+from detection.detection_model.abstract_detection_model import AbstractDetectionModel
+from detection.detection_model.model_factory import ModelFactory
+
+
+def load_valid_config():
+    return {
+        "model_type": "yolo",
+        "model_path": "src/detection/detection_model/model/test_model.pt"
+    }
+
+def load_fake_config():
+    return {
+        "model_type": "fake_model",
+        "model_path": "fake_path/model.pt"
+    }
+
+def test_model_factory_valid_config_returns_abstract_detection_model():
+    config = load_valid_config()
+    model = ModelFactory.get_model(config["model_type"], config["model_path"])
+    assert isinstance(model, AbstractDetectionModel)
+
+def test_model_factory_invalid_config_raises_exception():
+    config = load_fake_config()
+    try:
+        model = ModelFactory.get_model(config["model_type"], config["model_path"])
+        assert False, "Expected an exception to be raised for invalid model type"
+    except ValueError as e:
+        assert str(e) == f"Invalid model type: {config['model_type']}"
